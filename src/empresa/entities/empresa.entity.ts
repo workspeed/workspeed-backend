e9285bc676255dc141a128d151a892e6f2b78d36
eduardo <eduardo2005alve@gmail.com>
2026-07-16 21:45:12 -0300

    entidade e dto de empresa

---
+++ src/empresa/entities/empresa.entity.ts
@@ -0,0 +1,64 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { TipoPessoa } from '../enums/  tipoPessoa.enum';
+
+@Entity({ name: 'tb_empresa' })
+export class Empresa {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column({
+    name: 'nome_fantasia',
+    type: 'varchar',
+    length: 255,
+  })
+  nomeFantasia!: string;
+
+  @Column({
+    name: 'razao_social',
+    type: 'varchar',
+    length: 255,
+  })
+  razaoSocial?: string;
+
+  @Column({
+    type: 'varchar',
+    length: '14',
+    unique: true,
+  })
+  documento!: string;
+
+  @Column({
+    name: 'tipo_pessoa',
+    type: 'enum',
+    enum: TipoPessoa,
+  })
+  tipoPessoa!: TipoPessoa;
+
+  @Column({
+    type: 'varchar',
+    length: 11,
+  })
+  telefone!: string;
+
+  @Column({
+    type: 'varchar',
+    length: 200,
+  })
+  email?: string;
+
+  @CreateDateColumn({
+    name: 'dt_criacao',
+  })
+  dataCriacao!: Date;
+
+  @UpdateDateColumn({
+    name: 'dt_atualizacao',
+  })
+  dataAtualizacao!: Date;
+}
